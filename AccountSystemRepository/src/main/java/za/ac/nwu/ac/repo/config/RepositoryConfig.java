ae7b3915a7f59694e1ca438f35d4c71fcc1f4d83
tinus <tinusvanrooyen03@gmail.com>
2021-09-18 23:35:27 +0200

    DB repository (oracle error)

---
+++ AccountSystemRepository/src/main/java/za/ac/nwu/ac/repo/config/RepositoryConfig.java
@@ -0,0 +1,19 @@
+package za.ac.nwu.ac.repo.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.Entity;
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories("za.ac.nwu.domain.persistence")
+@EntityScan("za.ac.nwu.domain.persistence")
+@PropertySource(value = "classpath:application-db.properties")
+
+public class RepositoryConfig {
+
+
+}
